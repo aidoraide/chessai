4f6449f8b82f42d2c682bdc685f8dc6b7c1fc3b8
Aidan <aidoraide@gmail.com>
2019-12-13 15:18:28 -0800

    make GUI a bit more usable

--- play_vs_ai_server/play_vs_ai_server.py
+++ play_vs_ai_server/play_vs_ai_server.py
@@ -22,6 +22,8 @@ def get_colour_str(chess_colour):
     return 'black'
 
 
+nnet = load_model()
+nnet.eval() 
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -38,21 +40,24 @@ def start():
 def play():
     board = chess.Board(request.args['board'])
     player_colour = request.args['player_colour']
+    last_move = request.args.get('last_move')
+    last_move = chess.Move.from_uci(last_move) if last_move else ''
     selected_square = request.args.get('selected_square')
     chess_square = chess.SQUARE_NAMES.index(selected_square) if selected_square else None
     target_squares = []
 
-    if chess_square and board.piece_at(chess_square) and board.piece_at(chess_square).color == get_chess_colour(player_colour):
+    if chess_square is not None and board.piece_at(chess_square) and board.piece_at(chess_square).color == get_chess_colour(player_colour):
         targets = []
         for m in board.legal_moves:
             if m.from_square == chess_square:
                 targets.append(m.to_square)
                 target_squares.append(chess.SQUARE_NAMES[m.to_square])
         squares = chess.SquareSet(targets)
-        board_svg = chess.svg.board(board, squares=squares)
     else:
-        board_svg = chess.svg.board(board)
+        squares = None
     
+    board_svg = chess.svg.board(board, squares=squares, lastmove=last_move)
+
     target_squares = [f'"{t}"' for t in target_squares]
     game_over_message = ''
     if board.is_game_over():
@@ -69,6 +74,7 @@ def play():
         player_colour=player_colour,
         turn=get_colour_str(board.turn),
         game_over_message=game_over_message,
+        last_move=str(last_move)
     )
 
 @app.route('/play/action')
@@ -79,12 +85,14 @@ def action():
     to_square = request.args['to_square']
     move_str = from_square + to_square
 
+    move = None
     for m in board.legal_moves:
         if str(m) == move_str:
             board.push(m)
+            move = m
             break
 
-    urlParams = {'board': board.fen(), 'player_colour': player_colour}
+    urlParams = {'board': board.fen(), 'player_colour': player_colour, 'last_move': str(move)}
     return redirect('/play?' + urllib.parse.urlencode(urlParams))
 
 @app.route('/get_ai_move')
@@ -92,9 +100,7 @@ def get_ai_move():
     board = HashBoard(request.args['board'])
     player_colour = request.args['player_colour']
     print('Get AI move for board', board.fen())
-    nnet = load_model()
-    nnet.eval()
-    move = get_move_mcts(nnet, board, evaluation_mode=False)
+    move = get_move_mcts(nnet, board, evaluation_mode=True)
     from_square, to_square = chess.SQUARE_NAMES[move.from_square], chess.SQUARE_NAMES[move.to_square]
     return jsonify({
         'from_square': from_square,
@@ -102,4 +108,4 @@ def get_ai_move():
     })
 
 if __name__ == '__main__':
-    app.run(port=9999, debug=True)
+    app.run(port=9999, host='0.0.0.0', debug=True)
